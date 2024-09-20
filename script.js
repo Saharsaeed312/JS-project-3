36ac24aa8b681c340f0f932d918a68317f39bd52
Saharsaeed <saharsaeed8090@gmail.com>
2024-09-20 16:19:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var result = document.getElementById("result");
+document.getElementById("add").addEventListener("click",function(){
+    result.value = parseInt(num1.value)+parseInt(num2.value);
+})
+document.getElementById("sub").addEventListener("click",function(){
+    result.value = parseInt(num1.value)+parseInt(num2.value);
+})
+document.getElementById("mul").addEventListener("click",function(){
+    result.value = parseInt(num1.value)+parseInt(num2.value);
+})
+document.getElementById("div").addEventListener("click",function(){
+    result.value = parseInt(num1.value)+parseInt(num2.value);
+})
